765677fe247ee7ad4d93e06202ea37a3aa29bab1
Zuoweixiang <xiangzuowei@sensorsdata.cn>
2018-11-24 19:47:54 +0800

    test

--- android/src/androidTest/java/im/shimo/sa/ExampleInstrumentedTest.java
+++ android/src/androidTest/java/im/shimo/sa/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.sensorsdata.analytics;
+package im.shimo.sa;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sensorsdata.analytics.test", appContext.getPackageName());
+        assertEquals("im.shimo.sa.test", appContext.getPackageName());
     }
 }
